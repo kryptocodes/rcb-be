3c9346e4c4f31712655b5debf1f48df8f80b4afd
kryptocodes <srivatsantb@gmail.com>
2022-01-11 15:49:46 +0530

    added user verification and reverify end points
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -7,6 +7,7 @@ import { sendEmail } from "../middleware/mailer";
 
 import { getStatus } from "../controllers/Status";
 import { getUserByEmail } from "../controllers/Status";
+var ObjectID = require('mongodb').ObjectID;
 
 const router: Router = Router();
 
@@ -53,7 +54,6 @@ router.post(
         check("email").isEmail(),
     ],
     async (req: Request, res: Response) => {
-         // print the user object
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
@@ -67,6 +67,68 @@ router.post(
             const payload = {
                 email: user.email,
             };
+            res.status(200).json({
+                user
+            });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error");
+        }
+    }
+)
+
+router.post(
+    '/verify',
+    [
+        check("email").isEmail(),
+    ],
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { email } = req.body;
+        try {
+            let user: IUser = await User.findOne({ email });
+            if (!user) {
+                user = new User(email);
+              await user.save()
+            }
+            const payload = {
+                email: user.email,
+            };
+            sendEmail(email);
+            res.status(200).json({
+                user
+            });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error");
+        }
+    }
+)
+
+router.post(
+    "reverify",
+    [
+        check("email").isEmail(),
+    ],
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { email } = req.body;
+        try {
+            let user: IUser = await User.findOne({ email });
+            if (!user) {
+                user = new User(email);
+            await user.save()
+            }
+            const payload = {
+                email: user.email,
+            };
+            sendEmail(email);
             res.status(200).json({
                 user
             });
@@ -93,20 +155,29 @@ router.post(
             if (!user) {
                 return res.status(404).json({ msg: "User not found" });
             }
-            console.log(user)
-      
-            user.update(
-                {email: email},
-                {$set: req.body},
-                (err: any,User:any) => {
-                    if (err) {
-                        return res.status(500).json({ msg: "Server error" });
-                    }
-                    res.status(200).json({
-                        msg: "User updated",
-                    });
-                }
-            );
+            let id = user._id
+            await user.updateOne(
+                {"_id": id},
+                {$set: req.body}
+            ).then(() => {
+               res.status(200).json({
+                msg: "User created",
+                })
+            }).catch(err => {
+                res.status(500).send("Server Error");
+            })
+            
+            //     (err: any,User:any) => {
+            //         if (err) {
+            //             return res.status(500).json({ msg: "Server error" });
+            //         }
+            //         console.log(User)
+            //         res.status(200).json({
+            //             msg: "User updated",
+            //         });
+            //     }
+            // );
+           
         } catch (err) {
             console.error(err.message);
             res.status(500).send("Server Error");
